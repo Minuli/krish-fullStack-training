5addcf13c946adeedfdd5f9249401a6de0d06316
Minuli <minulilokuliyanage@gmail.com>
2022-04-11 12:30:50 +0530

    refactored graph nodes

--- breadth_first_search/src/Search_main.java
+++ breadth_first_search/src/Search_main.java
@@ -2,16 +2,16 @@ public class Search_main {
 
 
     public static void main(String[] args) {
-        Graph graph = new Graph(7);
+        Graph graph = new Graph(8);
 
         // generating the graph
         graph.addEdge(0,2);
         graph.addEdge(0,1);
         graph.addEdge(2, 6);
-        graph.addEdge(2, 4);
+        graph.addEdge(2, 7);
         graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
         graph.addEdge(4, 5);
-        graph.addEdge(4, 6);
 
         // invoking breadth first search traversal method
         graph.BreadthFirstSearch(0);

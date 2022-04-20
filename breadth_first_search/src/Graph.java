b2cc5d805e77b43e3d19bb5142a6a578b206e614
Minuli <minulilokuliyanage@gmail.com>
2022-04-20 13:08:30 +0530

    find articulation points using depth first search initial commit

--- breadth_first_search/src/Graph.java
+++ breadth_first_search/src/Graph.java
@@ -3,6 +3,7 @@ import java.util.LinkedList;
 public class Graph {
     private int noOfNodes;
     private LinkedList<Integer> adjacencyList[];
+    int total_time;
 
     //creating the graph
     public Graph(int n_nodes)
@@ -20,31 +21,75 @@ public class Graph {
     public void addEdge(int v,int w)
     {
         adjacencyList[v].add(w);
+        adjacencyList[w].add(v);
     }
 
-    public void hasPath(int edge, boolean nodes[])
+    public void hasPath(int c_edge, boolean nodes[],boolean articulationPoints[], int d[], int l[],int parent[])
     {
 
-        nodes[edge] = true; //all the visited nodes becomes true
-        int a;
+        nodes[c_edge] = true; //all the visited nodes becomes true
+        int a_edge;
         int i= 0;
-        System.out.print(edge + " ");
+        int children_count = 0;
+        d[c_edge] = l[c_edge] = ++total_time;
 
-        while (i<adjacencyList[edge].size())
+
+        while (i<adjacencyList[c_edge].size())
         {
-            a = adjacencyList[edge].get(i);
-            if(nodes[a]==false)   // if a is false means it is not visited
+            a_edge = adjacencyList[c_edge].get(i);
+            if(nodes[a_edge]==false)   // if a is false means it is not visited
             {
-                hasPath(a,nodes); // then recall the method to travers through it
+                parent[a_edge] = c_edge;
+                children_count++;
+                //System.out.println(children_count+"CHILD COUNT");
+                hasPath(a_edge,nodes,articulationPoints,d,l,parent); // then recall the method to travers through it
+                l[c_edge] = Math.min(l[c_edge],l[a_edge]); //find the minimum from current and the adjacency edge to check the parent-child connection
+
+                //checks for the root node
+                if (children_count>1)
+                {
+                    articulationPoints[c_edge] = true;
+                }
+                //checks for articulation points using l[v]>=d[u]
+                if (l[a_edge]>=d[c_edge])
+                {
+                    articulationPoints[c_edge] = true;
+                }
+
+
+            }
+            else
+            {
+                l[c_edge]=Math.min(l[c_edge],d[a_edge]);
             }
             i++;
         }
 
     }
 
-    public void DepthFirstSearch(int root)
+    public void findCutVertices_DFS()
     {
         boolean visited[] = new boolean[noOfNodes];
-        hasPath(root,visited);
+        boolean articulationPoints[] = new boolean[noOfNodes];
+        int min_time[] = new int[noOfNodes];
+        int parent[] = new int[noOfNodes];
+        int d_time[] = new int[noOfNodes];
+
+        for (int i=0; i < noOfNodes; i++)
+        {
+            if (!visited[i])
+            {
+                hasPath(i,visited,articulationPoints,d_time,min_time,parent);
+            }
+        }
+
+        for (int i=0;i<noOfNodes;i++)
+        {
+            if(articulationPoints[i]==true)
+            {
+                System.out.println(i+" ");
+            }
+        }
+
     }
 }
--- breadth_first_search/src/Search_main.java
+++ breadth_first_search/src/Search_main.java
@@ -4,21 +4,21 @@ public class Search_main {
 
 
     public static void main(String[] args) {
-        Graph graph = new Graph(8);
+        Graph graph = new Graph(6);
         Scanner scanner =  new Scanner(System.in);
 
         // generating the graph
-        graph.addEdge(0,2);
-        graph.addEdge(0,1);
-        graph.addEdge(2, 6);
-        graph.addEdge(2, 7);
-        graph.addEdge(1, 3);
-        graph.addEdge(1, 4);
-        graph.addEdge(4, 5);
+        graph.addEdge(1,0);
+        graph.addEdge(0,5);
+        graph.addEdge(1,3);
+        graph.addEdge(1,2);
+        graph.addEdge(2,3);
+        graph.addEdge(2,4);
+        graph.addEdge(3,4);
 
-        // invoking breadth first search traversal method
-        System.out.println("Select a number from [0,1,2,3,4,5,6,7]");
-        int root = scanner.nextInt();
-        graph.DepthFirstSearch(root);
+        // invoking FIND ARTICULATION POINT method
+        //System.out.println("Select a number from [0,1,2,3,4,5,6,7]");
+        //int root = scanner.nextInt();
+        graph.findCutVertices_DFS();
     }
 }
